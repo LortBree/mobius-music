d2ad940ff312c08d6438aff86674c72eac9084b5
bree <bree@Bree-Macbook.local>
2026-09-24 14:02:01 +0700

    Revert "Revert "Revert "v1.0.0: Rework UI"""
    This reverts commit deb77682b92abc0d75137f4c09aed7b9bbce00ca.

--- app/mobius/lib/app/theme/spacing.dart
+++
@@ -1,9 +0,0 @@
-abstract final class MobiusSpacing {
-  static const double xxs = 4;
-  static const double xs = 8;
-  static const double sm = 16;
-  static const double md = 24;
-  static const double lg = 32;
-  static const double xl = 48;
-  static const double xxl = 64;
-}
